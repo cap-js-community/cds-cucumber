9c7bc06a9084c782b719d04cb6efb647f33c145f
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-04-27 16:30:22 +0200

    replace cucumber.yml with env. variables

--- test/bin/testBookshop.sh
+++ test/bin/testBookshop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export CUCUMBER_PUBLISH_ENABLED=false
+export CUCUMBER_PUBLISH_QUIET=true
+
 export CDS_COMMAND=watch
 export TEST_SERVICE_DIR=tmp/cloud-cap-samples/fiori
+
 npx cucumber-js test/features/bookshop/read/tiles.feature
