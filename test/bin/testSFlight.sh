b25c3aff768d3d886bcbfa5454267b46558df9d8
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-04-27 16:37:07 +0200

    replace cucumber.yml, add docs folder (#6)

--- test/bin/testSFlight.sh
+++ test/bin/testSFlight.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export CUCUMBER_PUBLISH_ENABLED=false
+export CUCUMBER_PUBLISH_QUIET=true
+
 export TEST_USER=alice
 export TEST_PASSWORD=admin
 export TEST_SERVICE_DIR=tmp/cap-sflight
+
 npx cucumber-js test/features/sflight/travel.count.feature
