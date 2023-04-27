ce3817ff413279d90bc45ba435872969954bd98c
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-04-27 18:00:11 +0200

    add test getting-started

---
+++ test/bin/testGettingStarted.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+test -d tmp/getting-started && rm -r -f tmp/getting-started
+mkdir -p tmp/getting-started
+cd tmp/getting-started
+npm init -y
+npm i @sap/cds-dk
+rm package.json package-lock.json
+npx cds init --add samples,sqlite && npx cds deploy --to sqlite
+npm i git+https://github.com/cap-js/cds-cucumber-fe.git
+npx cds-add-cucumber-fe
+npx cucumber-js test
