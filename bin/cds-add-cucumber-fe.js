28f1e34c43b4869b4197dc2dfc3ac3454c2e6ff2
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-05-25 18:08:29 +0200

    Simplify basic search (#23)

--- bin/cds-add-cucumber-fe.js
+++ bin/cds-add-cucumber-fe.js
@@ -62,7 +62,7 @@ function createFirstFeatureFile() {
     Given we have started the application
       And we have opened the url "/"
     When we click on first Fiori preview page
-      And we perform basic search for "jane"
+      And we search for "jane"
     Then we expect to have 1 table records
 `
   const file = './test/features/first.feature';
