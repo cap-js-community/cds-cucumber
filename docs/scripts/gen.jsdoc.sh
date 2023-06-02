50e363a7aa25494ee16c1036e4bb4b6ee4dee529
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-06-02 16:32:38 +0200

    extend jsdoc

--- docs/scripts/gen.jsdoc.sh
+++ docs/scripts/gen.jsdoc.sh
@@ -2,20 +2,26 @@
 
 set -e
 set -x
-test -d tmp/jsdoc || mkdir -p tmp/jsdoc
+
+test -d tmp/jsdoc  || mkdir -p tmp/jsdoc
 
 pushd .
-
 cd tmp/jsdoc
-npm init -y
-npm i foodoc
-npm i jsdoc@3.6.11
+
+if [ -f package.json ]; then
+  echo Already initialized.
+else
+  npm init -y
+  npm i foodoc
+  npm i jsdoc@3.6.11
+  npm i showdown
+fi
 
 npx jsdoc -c ../../docs/scripts/jsdoc.config.json -t ./node_modules/foodoc/template -R ../../docs/README.md -r ../../lib/steps -d ../../docs -p ../../package.json
 
 for f in ../../docs/*.md; do
   fh="../../docs/${f:11:-3}.html"
-  npx showdown makehtml -i ${f} -o ${fh}
+  npx showdown makehtml -i ${f} -o ${fh} -p github
 done
 
 popd
