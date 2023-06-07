e43b16abd8f19cc1771290c0f812432e59a79477
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-06-07 12:03:23 +0200

    improve testGettingStarted.sh (#39)

--- test/bin/testGettingStarted.sh
+++ test/bin/testGettingStarted.sh
@@ -5,11 +5,18 @@ test -d tmp/getting-started && rm -r -f tmp/getting-started
 mkdir -p tmp/getting-started
 cd tmp/getting-started
 npm init -y
-npm i @sap/cds-dk
-rm package.json package-lock.json
+
+npm init -w dk -y
+npm i -w dk @sap/cds-dk
+
+npm init -w service -y
+
+cd service
+rm package.json
+
 npx cds init --add samples,sqlite && npx cds deploy --to sqlite
 npm i @sap/cds
 npm i express
-npm i git+https://$TOKEN@github.com/cap-js/cds-cucumber-fe.git#$BRANCH_NAME
+npm i -D git+https://$TOKEN@github.com/cap-js/cds-cucumber-fe.git#$BRANCH_NAME
 npx cds-add-cucumber-fe
 npx cucumber-js test
