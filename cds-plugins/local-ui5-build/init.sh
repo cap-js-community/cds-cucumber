56903f437bb57a150f3c5ba36473e36ab06176f6
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-06-16 16:06:48 +0200

    enable

--- cds-plugins/local-ui5-build/init.sh
+++ cds-plugins/local-ui5-build/init.sh
@@ -3,7 +3,5 @@ set -x
 echo "---------------- init.sh ----------------"
 pwd
 
-npm init -w ui5cli -y
-npm i -w ui5cli @ui5/cli
-
+npm i @ui5/cli
 npx ui5 build preload --include-task generateVersionInfo --all
