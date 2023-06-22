672bd00dde3f5cf255e4d49cdcbf8b07557d05d8
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-06-22 09:23:22 +0200

    build ui5 cache

---
+++ test/bin/build.ui5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR=tmp/sapui5/full-1.110.1
+
+#test -d ${DIR} && rm -r -f ${DIR}
+test -d ${DIR} || mkdir -p ${DIR}
+
+cp -v -r cds-plugins/local-ui5-build/files/* ${DIR}
+ls -altr ${DIR}
+
+cd ${DIR}
+
+test -f package.json || npm init -y
+test -d node_modules/@ui5/cli || npm i @ui5/cli
+test -d dist || npx ui5 build preload --all --include-task generateVersionInfo
+#npx ui5 build self-contained --all --include-task generateVersionInfo
+
+ls -altr .
