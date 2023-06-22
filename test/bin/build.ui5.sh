2a30d1b0f763d177504a05e1d5b0e931204629df
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-06-22 18:40:19 +0200

    build any ui5 version (#71)

--- test/bin/build.ui5.sh
+++ test/bin/build.ui5.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-DIR=tmp/sapui5/full-1.110.1
+set -e
+set -x
 
-#test -d ${DIR} && rm -r -f ${DIR}
+. ./test/bin/.sapui5.version.sh
+
+DIR=tmp/sapui5/full-${SAP_UI5_VERSION}
+
+test -d ${DIR} && rm -r -f ${DIR}
 test -d ${DIR} || mkdir -p ${DIR}
 
 cp -v -r cds-plugins/local-ui5-build/files/* ${DIR}
@@ -12,6 +17,8 @@ cd ${DIR}
 
 test -f package.json || npm init -y
 test -d node_modules/@ui5/cli || npm i @ui5/cli
+npx ui5 use sapui5@${SAP_UI5_VERSION}
+npm version "${SAP_UI5_VERSION}"
 #test -d dist || npx ui5 build preload --all --include-task generateVersionInfo
 test -d dist || npx ui5 build self-contained --all --include-task generateVersionInfo
 
