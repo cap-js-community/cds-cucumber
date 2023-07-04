2be2a4cce9d44e54980f38fb09ab645f02d1b048
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-07-04 17:15:00 +0200

    bookshop needs sqlite3 (#88)

--- test/bin/prepare.sh
+++ test/bin/prepare.sh
@@ -5,7 +5,7 @@ cd tmp
 
 test -d cloud-cap-samples || git clone https://github.com/SAP-samples/cloud-cap-samples.git --single-branch
 pushd .
-cd cloud-cap-samples && npm i && npm i @sap/cds-dk
+cd cloud-cap-samples && npm i sqlite3
 popd
 
 test -d cap-sflight || git clone https://github.com/SAP-samples/cap-sflight.git --single-branch
