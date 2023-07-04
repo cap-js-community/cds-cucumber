292cc4d0404bc525ba2c57fccffa830364bf31a1
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-07-04 14:20:25 +0200

    local ui5 build plugin: set version

--- cds-plugins/local-ui5-build/init.sh
+++ cds-plugins/local-ui5-build/init.sh
@@ -5,9 +5,13 @@ ARGS="$@"
 set -x
 set -e
 
+. ${CDS_CUCUMBER_PLUGIN_DIR}/.sapui5.version.sh
+
 if [ "${SAPUI5_DIST_DIRECTORY}" = "" ]; then
   echo "Build UI5"
   npm i @ui5/cli
+  npx ui5 use sapui5@${SAP_UI5_VERSION}
+  npm version ${SAP_UI5_VERSION}
   # ./build.preload.sh
   ./build.self-contained.sh
 else  
