1dcda3d3d4661a463e40fbad278ba460ca930c91
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-07-05 09:53:43 +0200

    testGettingStarted.sh: support cds version passed as environment variable

--- test/bin/testGettingStarted.sh
+++ test/bin/testGettingStarted.sh
@@ -3,7 +3,12 @@
 set -e
 set -x
 
-DIR=tmp/getting-started-700
+if [ "${CDS_VERSION}" == "" ]; then
+  export CDS_VERSION="^7.0.0"
+fi
+
+CDS_VERSION_SHORT=${CDS_VERSION//[\^\~\.]/}
+DIR=tmp/getting-started-${CDS_VERSION_SHORT}
 
 test -d $DIR && rm -r -f $DIR
 test -d ${DIR} || mkdir -p $DIR
@@ -12,17 +17,25 @@ cd $DIR
 npm init -y
 
 npm init -w dk -y
-npm i -w dk @sap/cds-dk@^7.0.0
+npm i -w dk @sap/cds-dk@${CDS_VERSION}
 
 npm init -w service -y
 
 cd service
 rm package.json
 
-npx cds init --add samples,sqlite && npx cds deploy --to sqlite
-npm i @sap/cds@^7.0.0
+npx cds init
+if [ "${CDS_VERSION_SHORT::1}" = "6" ]; then
+  npx cds add samples
+else
+  npx cds add sample
+fi
+npx cds add sqlite
+npx cds deploy --to sqlite
+
+npm i @sap/cds@${CDS_VERSION}
 npm i express
-if [ "$BRANCH_NAME" == "" ]; then
+if [ "$BRANCH_NAME" = "" ]; then
   npm i ../../.. || true
 else
     npm i -D git+https://github.com/cap-js-community/cds-cucumber.git#$BRANCH_NAME
@@ -30,7 +43,7 @@ else
 fi
 npx cds-add-cucumber
 
-if [ "$BRANCH_NAME" == "" ]; then
+if [ "$BRANCH_NAME" = "" ]; then
 
 cat <<EOF >cucumber.yml
 default:
