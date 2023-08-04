b33b58988676c6de38b3e93a6d0ef927075cad6b
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-08-04 10:52:55 +0200

    improve changelog, add UI5 version: (#110)
    - fix changelog typos
    - add more SAPUI5 versions

--- test/bin/build.ui5.all.sh
+++ test/bin/build.ui5.all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 SAP_UI5_VERSION="1.110.1" ./test/bin/build.ui5.sh
-SAP_UI5_VERSION="1.111.4" ./test/bin/build.ui5.sh
+SAP_UI5_VERSION="1.111.8" ./test/bin/build.ui5.sh
 SAP_UI5_VERSION="1.112.3" ./test/bin/build.ui5.sh
 SAP_UI5_VERSION="1.113.0" ./test/bin/build.ui5.sh
-SAP_UI5_VERSION="1.114.0" ./test/bin/build.ui5.sh
-SAP_UI5_VERSION="1.115.0" ./test/bin/build.ui5.sh
+SAP_UI5_VERSION="1.114.2" ./test/bin/build.ui5.sh
+SAP_UI5_VERSION="1.115.1" ./test/bin/build.ui5.sh
+SAP_UI5_VERSION="1.116.0" ./test/bin/build.ui5.sh
