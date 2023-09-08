08de8bcad26b66ae20d919d85c9c6688386089b3
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-09-08 13:49:15 +0200

    improve odata

--- cds-plugins/local-ui5-build/.sapui5.version.sh
+++ cds-plugins/local-ui5-build/.sapui5.version.sh
@@ -1,8 +1,10 @@
-if [ "${SAP_UI5_VERSION}" == "" ]; then
+if [ -z "${SAP_UI5_VERSION}" ]; then
   #export SAP_UI5_VERSION="1.110.1"
-  #export SAP_UI5_VERSION="1.111.4"
+  #export SAP_UI5_VERSION="1.111.8"
   #export SAP_UI5_VERSION="1.112.3"
   #export SAP_UI5_VERSION="1.113.0"
-  export SAP_UI5_VERSION="1.114.0"
-  #export SAP_UI5_VERSION="1.115.0"
+  #export SAP_UI5_VERSION="1.114.2"
+  #export SAP_UI5_VERSION="1.115.1"
+  #export SAP_UI5_VERSION="1.116.0"
+  export SAP_UI5_VERSION="1.117.1"
 fi
