244a73613d04cc1e2d4c0a2ab61da9c2edcc71c5
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-10-10 16:23:20 +0200

    update SAPUI5 versions (#151)
    - remove duplicated crud test
    - add scripts to test all SAPUI5 versions
    - add recommended vscode extension

---
+++ test/bin/testBookshop.All.Versions.PARALLEL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for dir in ls ./tmp/sapui5/full*
+do
+  VERSION="${dir:18}"
+  if [ -n "${VERSION}" ]; then
+    NOW=`date +%d.%m.%Y-%H.%M.%S`
+    LOGFILENAME="/tmp/cds-cucumber-${VERSION}-${NOW}.log"
+    echo "test ${VERSION} ${LOGFILENAME}"
+    SAP_UI5_VERSION=${VERSION} test/bin/testBookshopWithLocalUI5plugin.sh &> ${LOGFILENAME}
+    echo $?
+  fi
+done
