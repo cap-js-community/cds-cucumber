708b6f57dca9b19b2f2b2b86cf8538c6464153a7
Vladislav Leonkev <vladislav.leonkev@sap.com>
2023-10-17 15:15:52 +0200

    update dependencies - update SAP UI5 versions - fix expectation step

--- cds-plugins/local-ui5-build/.sapui5.version.sh
+++ cds-plugins/local-ui5-build/.sapui5.version.sh
@@ -3,9 +3,10 @@ if [ -z "${SAP_UI5_VERSION}" ]; then
   #export SAP_UI5_VERSION="1.111.8"
   #export SAP_UI5_VERSION="1.112.3"
   #export SAP_UI5_VERSION="1.113.0"
-  #export SAP_UI5_VERSION="1.114.6"
+  #export SAP_UI5_VERSION="1.114.8"
   #export SAP_UI5_VERSION="1.115.1"
   #export SAP_UI5_VERSION="1.116.0"
-  #export SAP_UI5_VERSION="1.117.2"
-  export SAP_UI5_VERSION="1.118.1"
+  #export SAP_UI5_VERSION="1.117.3"
+  #export SAP_UI5_VERSION="1.118.1"
+  export SAP_UI5_VERSION="1.119.1"
 fi
