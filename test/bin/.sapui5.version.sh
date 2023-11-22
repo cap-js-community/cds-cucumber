eec362ad10b330ba73ed8b30ab800e7f4c23ac12
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2023-11-22 17:31:41 +0100

    add ui5 1.114.10 (#167)

--- test/bin/.sapui5.version.sh
+++ test/bin/.sapui5.version.sh
@@ -3,7 +3,7 @@ if [ -z "${SAP_UI5_VERSION}" ]; then
   #export SAP_UI5_VERSION="1.111.8"
   #export SAP_UI5_VERSION="1.112.3"
   #export SAP_UI5_VERSION="1.113.0"
-  #export SAP_UI5_VERSION="1.114.9"
+  #export SAP_UI5_VERSION="1.114.10"
   #export SAP_UI5_VERSION="1.115.1"
   #export SAP_UI5_VERSION="1.116.0"
   #export SAP_UI5_VERSION="1.117.4"
