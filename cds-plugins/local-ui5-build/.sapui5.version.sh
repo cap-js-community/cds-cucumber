a419d9338d0669a8ae5fbe5b1fd7e629edfb1add
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2024-03-06 10:44:53 +0100

    update versions (#195)

--- cds-plugins/local-ui5-build/.sapui5.version.sh
+++ cds-plugins/local-ui5-build/.sapui5.version.sh
@@ -10,5 +10,5 @@ if [ -z "${SAP_UI5_VERSION}" ]; then
   #export SAP_UI5_VERSION="1.118.1"
   #export SAP_UI5_VERSION="1.119.2"
   #export SAP_UI5_VERSION="1.120.9"
-  export SAP_UI5_VERSION="1.121.0"
+  export SAP_UI5_VERSION="1.121.1"
 fi
