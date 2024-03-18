33ba798c6977b6d0c2b3982b5457a712cb7dfe6d
Vladislav Leonkev <vladislav.leonkev@sap.com>
2024-03-18 13:20:21 +0100

    update versions

--- cds-plugins/local-ui5-build/.sapui5.version.sh
+++ cds-plugins/local-ui5-build/.sapui5.version.sh
@@ -9,6 +9,6 @@ if [ -z "${SAP_UI5_VERSION}" ]; then
   #export SAP_UI5_VERSION="1.117.5"
   #export SAP_UI5_VERSION="1.118.1"
   #export SAP_UI5_VERSION="1.119.2"
-  #export SAP_UI5_VERSION="1.120.9"
-  export SAP_UI5_VERSION="1.121.1"
+  #export SAP_UI5_VERSION="1.120.10"
+  export SAP_UI5_VERSION="1.121.2"
 fi
