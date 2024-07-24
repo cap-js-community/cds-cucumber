b26ac173cc12aacf3ed1771ad6937475456e61f6
Vladislav Leonkev <131776471+vl-leon@users.noreply.github.com>
2024-07-24 16:10:11 +0200

    update versions (#222)

--- test/bin/build.ui5.all.tgz.sh
+++ test/bin/build.ui5.all.tgz.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SAP_UI5_VERSION="1.110.1" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.111.8" ./test/bin/build.ui5.tgz.sh
-SAP_UI5_VERSION="1.112.3" ./test/bin/build.ui5.tgz.sh
-SAP_UI5_VERSION="1.113.0" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.114.14" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.115.1" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.116.0" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.117.5" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.118.1" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.119.2" ./test/bin/build.ui5.tgz.sh
-SAP_UI5_VERSION="1.120.13" ./test/bin/build.ui5.tgz.sh
-SAP_UI5_VERSION="1.121.4" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.120.18" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.121.6" ./test/bin/build.ui5.tgz.sh
 SAP_UI5_VERSION="1.122.2" ./test/bin/build.ui5.tgz.sh
-SAP_UI5_VERSION="1.123.1" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.123.2" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.124.2" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.125.1" ./test/bin/build.ui5.tgz.sh
+SAP_UI5_VERSION="1.126.1" ./test/bin/build.ui5.tgz.sh
